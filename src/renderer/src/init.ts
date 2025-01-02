eff229ab79788b9d598d759cbbf2eacf0e625d16
kangfenmao <kangfenmao@qq.com>
2025-01-02 09:21:34 +0800

    feat: add mini window

--- src/renderer/src/init.ts
+++ src/renderer/src/init.ts
@@ -1,6 +1,18 @@
 import KeyvStorage from '@kangfenmao/keyv-storage'
 
 function init() {
+  initSpinner()
+  initKeyv()
+}
+
+function initSpinner() {
+  const spinner = document.getElementById('spinner')
+  if (spinner && window.location.hash !== '#/mini') {
+    spinner.style.display = 'flex'
+  }
+}
+
+function initKeyv() {
   window.keyv = new KeyvStorage()
   window.keyv.init()
 }
